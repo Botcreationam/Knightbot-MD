5a77dbc2c14f83401e8e782e769f222ad6236fa1
Botcreationam <130276660+Botcreationam@users.noreply.github.com>
2025-03-22 11:15:30 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: '+260978277538',
+  author: 'FRANKLIN STICK 🍼',
+  botName: "FRANKLIN",
+  botOwner: 'FRANKLIN', // Your name
+  ownerNumber: '260978277538', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
